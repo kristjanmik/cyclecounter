5c376288a977198759fce56bdd3837fffa765f84
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-08-08 20:48:47 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,88 +1,26 @@
 var request = require('request');
-var cheerio = require('cheerio');
+var parseString = require('xml2js').parseString;
 var err = require('error-helper');
 
-
-exports.is = function (options, callback) {
-
-	var queryString = {
-		nafn: options.name || '',
-		heimili: options.address || '',
-		kt: options.socialnumber || '',
-		vsknr: options.vsknr || ''
-	};
-
+exports.is = function (callback) {
 	request.get({
-		url: 'http://www.rsk.is/fyrirtaekjaskra/leit',
-		qs: queryString
-	}, function (error, response, body) {
-		if (error || response.statusCode !== 200) {
-			return callback(err(502,'www.us.is refuses to respond or give back data'));
+		url: 'http://www.bicyclecounter.dk/BicycleCounter/GetCycleInfo?ran=1379500208853&StationId=235&LaneId=0'
+	}, function(err, response, xml) {
+		if(err || response.statusCode !== 200){
+			return callback(err(500,'www.bicyclecounter.dk refuses to respond or give back data'));
 		}
-		var $ = cheerio.load(body),
-			obj = {
-				results: []
-			},
-			car = {};
+		var cyclecounter = [];
+		parseString(xml, { explicitRoot: false }, function(err, result) {
 
-		var fields = ['registryNumber', 'number', 'factoryNumber', 'type', 'subType', 'color', 'registeredAt', 'status', 'nextCheck', 'pollution', 'weight'];
-		var nothingFound = $('table tr td').html();
-
-		if (nothingFound.indexOf('Ekkert ökutæki fannst') > -1) {
-			return callback(err(404,'License plate not found'));
-		}
-
-		var obj = {
-			results: []
-		},
-			$ = cheerio.load(body);
-
-		function cleanHtml(input) {
-			var html = input.html();
-			if (!html) {
-				return '';
-			}
-			return html.replace(/<(?:.|\n)*?>/gm, '');
-		};
-
-		if ($('.resultnote').length == 0) {
-			console.log('here')
-			var tr = $('.boxbody > .nozebra tbody tr');
-			if (tr.length > 0) {
-				var name = $('.boxbody > h1').html(),
-					sn = $('.boxbody > h1').html();
-
-				obj.results.push({
-					name: name.substring(0, name.indexOf('(') - 1),
-					sn: sn.substring(sn.length - 11, sn.length - 1),
-					active: $('p.highlight').text().length === 0 ? 1 : 0,
-					address: cleanHtml(tr.find('td').eq(0))
-				});
-			}
-		} else {
-			var table = $('table tr');
-			table = table.slice(1, table.length);
-
-			table.each(function () {
-
-				var td = $(this).find('td');
-				var nameRoot = cleanHtml(td.eq(1));
-				var felagAfskrad = "(Félag afskráð)";
-
-				obj.results.push({
-					name: nameRoot.replace("\n", "").replace(felagAfskrad, "").replace(/^\s\s*/, '').replace(/\s\s*$/, ''),
-					sn: cleanHtml(td.eq(0)),
-					active: nameRoot.indexOf(felagAfskrad) > -1 ? 0 : 1,
-					address: cleanHtml(td.eq(2))
-				});
-
+			cyclecounter.push({
+				DayCount: result.DayCount[0],
+				YearCount: result.YearCount[0],
+				Time: result.Time[0],
+				Date: result.Date[0],
 			});
-		}
 
-		obj.results = obj.results.filter(function (result) {
-			return result.active
-		})
-
-		return callback(null, obj)
+		});
+		
+		return callback(null,{results: cyclecounter});
 	});
 };
